ceae9b22b53378ce8216198c5b70d57908335cef
Bogdan Mic <bogdan.mic@trustyou.net>
2017-03-19 14:17:46 +0200

    Added logging

--- scrape.py
+++ scrape.py
@@ -5,10 +5,13 @@ import os
 import sys
 import json
 import socket
+import logging
 import requests
 import traceback
 from collections import deque
 
+logging.basicConfig()
+logger = logging.getLogger()
 
 def start_crawling(req_obj, proxies={}):
     start_url = req_obj.get("url")
@@ -23,6 +26,7 @@ def start_crawling(req_obj, proxies={}):
         req = dict()
         url = urls.popleft()
         try:
+            logger.info("Requesting:\t%s", url)
             res = session.request("GET", url, timeout=150, cookies=None, verify=False,
                                   headers={
                                       "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16",
@@ -48,6 +52,7 @@ def start_crawling(req_obj, proxies={}):
             continue
 
         try:
+            logger.info("Extracting urls:\t%s", req["url"])
             crawled = extractor.extract_urls(req)
         # print crawled
         except Exception as ex:
@@ -60,6 +65,7 @@ def start_crawling(req_obj, proxies={}):
                 urls.append(page)
 
         try:
+            logger.info("Extracting data:\t%s", req["url"])
             data["data"] = extractor.extract_data(req)
             data["start_url"] = req["url"]
             data["html"] = req["html"]
